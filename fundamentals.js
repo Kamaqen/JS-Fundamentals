a1104ad22dc08211d7a2e507b98243897d40d5fe
“Kamaqen” <jmcapcha@gmail.comgit config --global core.autocrlf inputgit config --global core.safecrlf truegit config --global core.editor code>
2023-08-09 14:27:17 -0500

    ADD: initial commit

---
+++ fundamentals.js
@@ -0,0 +1,11 @@
+// console.log("Hello, World!");
+// let username = 'Testino';
+// console.log(`Welcome, ${username}!!`);
+// for(let index = 0; index < 80; index++) {
+//     console.log(`Inside the loop times ${index}!!`);
+// };
+let admin,
+    name;
+name = "John";
+admin = name;
+alert(admin)
