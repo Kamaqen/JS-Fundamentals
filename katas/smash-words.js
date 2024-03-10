5be92c050d875e5e41919c738653049e5d754b7b
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-03-10 10:57:28 -0500

    UPDATE: extra solution for kata smash words

--- katas/smash-words.js
+++ katas/smash-words.js
@@ -1,3 +1,15 @@
 smash = function (words) {
   return words.join(" ");
 };
+
+function smash(words) {
+  "use strict";
+  var smashed = "";
+  for (var i = 0; i < words.length; i++) {
+    smashed += words[i];
+    if (i != words.length - 1) {
+      smashed += " ";
+    }
+  }
+  return smashed;
+}
